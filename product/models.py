b3bf0ddc872650bb727fa398037a0beb4996bff7
aminhesami-1 <amin12hesami@gmail.com>
2024-01-27 12:50:22 +0330

    file admin.py and model.py : admin.py :registered the new model named 'ProductCategory' that connected to Categorey field in model named product by many to many rel models.py : added ProductCategory model that include title field that has char field with 64 max length the title fildes values goes to category field in prudoct model and i migrate the models and fields and tested the rels whoch all work masage: migrate files has no commit becuse the files has not tracked by git

--- product/models.py
+++ product/models.py
@@ -1,16 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class ProductCategory(models.Model):
+    title = models.CharField(max_length=64)
+    def __str__(self):
+        return self.title 
+
 class Product(models.Model):
     title = models.CharField(max_length=300)
     price = models.IntegerField()
     slug = models.SlugField()
-    category = models.ManyToManyField()
+    category = models.ManyToManyField(ProductCategory)
     is_active = models.BooleanField()
     is_delete = models.BooleanField(default= False)
-    short_description =models.TextField(max_length=260)
+    short_description =models.CharField(max_length=260)
     description = models.TextField(help_text="descibe the product here")
 
 
     def __str__(self):
-        return self.title
+        return self.title 
+    
+
