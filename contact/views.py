22eff5b1335fa209cdb9fe22dd6d9f483b2d2961
aminhesami-1 <amin12hesami@gmail.com>
2024-02-02 16:24:50 +0330

    file contact/views.py : added add view to add data that come from contact form and add to DB

--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render , HttpResponseRedirect 
+from django.urls import reverse
+from .models import ContactUs
 from .forms import ContactForm
 
 def contact(request):
@@ -7,3 +9,16 @@ def contact(request):
         print(form.is_valid())
         print(form.cleaned_data)
     return render(request,'contact/index.html', {'form':ContactForm})
+
+
+
+def add(request):
+    if request.method == "POST":
+        full_name = request.POST.get('full_name')
+        message = request.POST.get('message')
+        subject = request.POST.get('subject')
+        phone = request.POST.get('phone')
+        email = request.POST.get('email')
+        Cform  = ContactUs(full_name=full_name , subject=subject , message=message , phone=phone , email=email)
+        Cform.save()
+    return HttpResponseRedirect(reverse("lightshop:contact"))
