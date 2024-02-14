4d4a56f696e6b460f2535f204a13768aee1731f8
bill <bill.bkh@gmail.com>
2024-02-14 21:02:57 +0330

    coustom User model created

--- lightshop/settings.py
+++ lightshop/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'shop',
     'product',
     'contact',
+    'account',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -129,3 +130,6 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'account.User'
--- lightshop/urls.py
+++ lightshop/urls.py
@@ -20,10 +20,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
     path('product/', include('product.urls')),
-    path('contact/', include('contact.urls'))
+    path('contact/', include('contact.urls')),
+    path('account/', include('account.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
