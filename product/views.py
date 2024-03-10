61ff2eb350e21495c2064cff83b18078d0933d3e
Bill-BKH <bill.bkh@gmail.com>
2024-03-10 19:13:45 +0330

    fixed paginator problem for showing product page

--- product/views.py
+++ product/views.py
@@ -6,13 +6,11 @@ from django.core.paginator import Paginator
 # Create your views here.
 def product_detail(request, slug):
     data = Product.objects.get(slug=slug)
-
-
     return render(request, 'product/single-product.html',{'product':data})
 
 def home(request):
     products = Product.objects.all().order_by()
-    paginator = Paginator(products, 2)
-    page_obj = paginator.get_page(request.GET)
+    paginator = Paginator(products, 4)
+    page_obj = paginator.get_page(request.GET.get('page', 1))
 
     return render(request,'product/home.html',{'page_obj':page_obj, 'paginator':paginator})
