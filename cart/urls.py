6ee6b53da8fdd3bbfbae0ff1510b4d9bdf54b3b4
aminhesami-1 <amin12hesami@gmail.com>
2024-03-11 18:03:35 +0330

    file   cart/urls.py : added urls for views page functions

--- cart/urls.py
+++ cart/urls.py
@@ -3,9 +3,11 @@ from . import views
 
 app_name='cart'
 urlpatterns = [
-    path('', views.cart , name='contact'),
+    path('', views.cart , name='cart'),
     path('addtocart/', views.add_to_cart, name='add_to_cart'),
-    path('deleteformcart/', views.delete_form_cart, name='delete_form_cart')
+    path('deleteformcart/', views.delete_form_cart, name='delete_form_cart'),
+    path('addcount/', views.add_count, name='add_count'),
+    path('minuscount/', views.minus_count, name='minus_count')
 
 ]
 
